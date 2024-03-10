1c04153fd6715ec9bdb2180eb310572445bcf2ea
Luies <31912290+cnuvasan@users.noreply.github.com>
2024-03-10 05:50:56 -0600

    Create Azure_Resource_Group.tf

---
+++ Azure_Resource_Group.tf
@@ -0,0 +1,27 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "tf-rg"
+  location = "westus2"
+}
+
+
+terraform init
+terraform validate
+terraform test
+terraform deploy
+terraform destroy
